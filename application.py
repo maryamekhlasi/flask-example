bf5aca0ea8f1462a820d970147af41a8a917a321
maryamekhlasi <38199297+maryamekhlasi@users.noreply.github.com>
2021-09-28 17:29:49 -0400

    Update application.py

--- application.py
+++ application.py
@@ -3,9 +3,8 @@ import requests
 application = Flask(__name__)
 
 
-response = requests.get('http://169.254.169.254/latest/meta-data/instance-id')
-instance_id = response.text
+instanceid = urllib.request.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read().decode()
 
 @application.route('/')
 def hello_world():
-	return instance_id
+	return instanceid

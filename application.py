eb2c1654d6f24cee65ec8ea1179c51eb7f16e302
maryamekhlasi <38199297+maryamekhlasi@users.noreply.github.com>
2021-09-28 17:57:25 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,13 @@
 from flask import Flask
+import boto3
 application = Flask(__name__)
+
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_instances()
+
 
 
 @application.route('/')
 def hello_world():
-	return '"name","Maryam"'
+	return response["Reservations"][0]["Instances"][0]["InstanceId"]
